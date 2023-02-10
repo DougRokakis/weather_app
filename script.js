f5fc9413961abcf3bc44ed598b16033d1301a3eb
unknown <doug.rokakis@gmail.com>
2023-02-10 16:05:09 -0500

    resolveing http/https issue

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let weather = {
     apiKey: "372ef8064c25941b9373e60843ea0bff",
     fetchWeather: function (city) {
         fetch(
-            "http://api.openweathermap.org/data/2.5/weather?q="
+            "https://api.openweathermap.org/data/2.5/weather?q="
             + city
             + "&units=metric&appid="
             + this.apiKey)
